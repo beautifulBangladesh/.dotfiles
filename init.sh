7919f600c2a76d3d2942cffc8a1d94035c8134be
Beautiful Bangladesh <rafeuddarajofficial@gmail.com>
2025-05-08 08:29:26 +0000

    feat: gitpod dotfile config

--- init.sh
+++ init.sh
@@ -2,16 +2,17 @@
 
 echo "Dotfiles init.sh running..."
 
-GITPOD_HOST=$(echo $GITPOD_WORKSPACE_URL | sed 's|https://||' | sed 's|/||g')
+# Extract Gitpod Workspace URL, without https:// and /
+GITPOD_HOST=$(echo $GITPOD_WORKSPACE_URL | sed 's|https://||' | sed 's|/.*||')
 
 CONFIG_PATH="vite.config.js"
 
 if [ -f "$CONFIG_PATH" ]; then
   echo "🔧 Injecting Gitpod host: $GITPOD_HOST into vite.config.js..."
 
-
+  # Check if the host is already in vite.config.js
   if ! grep -q "$GITPOD_HOST" "$CONFIG_PATH"; then
-  
+    # Inject the allowedHosts property
     sed -i "/server: {/a \ \ \ \ allowedHosts: ['$GITPOD_HOST']," "$CONFIG_PATH"
     echo "✅ Injection done!"
   else
